061396d8563e3352e29f174a8f510a0444cfa8fb
Ruana Rodriguez Rueda <Ruana@Georges-MacBook-Air.local>
2020-05-08 04:51:17 +0200

    References converted, added bibliography page

--- convert.js
+++ convert.js
@@ -6,10 +6,10 @@ const { replaceRefs } = require('./util')
 var files = fs.readdirSync('.').filter(f => path.extname(f) === '.tex')
 
 files.forEach(f => {
-    // fs.createReadStream(f).pipe(fs.createWriteStream(`${f}.bak`));
+    fs.createReadStream(f).pipe(fs.createWriteStream(`${f}.bak`));
 
     var text = fs.readFileSync(f, 'utf8')
     var citations = replaceRefs(text)
 
-    fs.writeFileSync(`./_${f}`, citations)
+    fs.writeFileSync(`./${f}`, citations)
 })
